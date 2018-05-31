87ee3cc7d6f76de94d6328d54d7f726d20a46341
ReBuilders101 <rebuilders101@gmail.com>
2018-05-31 15:26:12 +0200

    Added the ResourceContainer interface, which represents a folder

---
+++ src/dev/lb/cellpacker/structure/ResourceContainer.java
@@ -0,0 +1,53 @@
+package dev.lb.cellpacker.structure;
+
+import java.util.List;
+
+import dev.lb.cellpacker.Utils;
+import dev.lb.cellpacker.structure.resource.Resource;
+
+public interface ResourceContainer {
+	
+	public List<Resource> getResources();
+	public List<ResourceContainer> getSubCategories();
+	public String getName();
+	
+	public default Resource getResource(int index){
+		return getResources().get(index);
+	}
+
+	public default Resource getResource(String name){
+		return Utils.getFirst(getResources(), (r) -> r.getName().equals(name));
+	}
+
+	public default ResourceContainer getSubCategory(int index){
+		return getSubCategories().get(index);
+	}
+
+	public default ResourceContainer getSubCategory(String name){
+		return Utils.getFirst(getSubCategories(), (c) -> c.getName().equals(name));
+	}
+	
+	public default void addResource(Resource r){
+		getResources().add(r);
+	}
+	
+	public default void addSubCategory(ResourceContainer c){
+		getSubCategories().add(c);
+	}
+	
+	public default boolean hasResources(){
+		if(getResources() == null){
+			return false;
+		}else{
+			return !getResources().isEmpty();
+		}
+	}
+	
+	public default boolean hasSubCategories(){
+		if(getSubCategories() == null){
+			return false;
+		}else{
+			return !getSubCategories().isEmpty();
+		}
+	}
+}
